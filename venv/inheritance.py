33ea87bc70badcb09ea571e2c45336d1efc5215d
Akshayan442 <80045465+Akshayan442@users.noreply.github.com>
2021-03-25 08:13:57 +0400

    Delete inheritance.py

--- venv/inheritance.py
+++
@@ -1,8 +0,0 @@
-class test:
-    def __init__(self):
-        self.m1 = 50
-        self.m2 = 40
-class grade(test):
-    def __init__(self):
-        self.total = self.m1 + self.m2
-        print(self.total)
